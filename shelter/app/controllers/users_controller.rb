5e53ca0ba63dd27ff373157eb68ad87628bf2396
shishkina <shishkina.kate@gmail.com>
2015-11-06 11:27:08 -0500

    fixing things

--- shelter/app/controllers/users_controller.rb
+++ shelter/app/controllers/users_controller.rb
@@ -1,2 +1,45 @@
 class UsersController < ApplicationController
+  skip_before_action :authenticate, only: [:new,:create,:show]
+
+def new
+  @user = User.new
+end
+
+def index
+  @users = User.all
+end
+
+def show
+  if logged_in?
+
+    @user = User.find(session[:user_id])
+  #  if  @user.roles.find{|role| role=='administrator'}
+  #   redirect some special admin page
+  #   do something fo the admin
+  else
+    redirect_to login_path
+  end
+end
+
+def create
+  @user = User.new user_params
+  if @user.save
+    flash[:success] = "Welcome to THE SHELTER"
+    redirect_to @user
+  else
+    render new_user_path
+  end
+end
+
+def destroy
+  @user = User.find params[:id]
+  @user.destroy
+  redirect_to users_path
+end
+
+private
+
+def user_params
+  params.require(:user).permit(:name, :phone, :admin, :email, :password, :password_confirmation)
+end
 end
--- shelter/app/models/user.rb
+++ shelter/app/models/user.rb
@@ -1,10 +1,12 @@
 class User < ActiveRecord::Base
   has_many :animals
 
-  validates :name
+  validates :name, presence: true
   EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: {maximum: 55}, format: {with: EMAIL_REGEX}, uniqueness: {case_sensitive: false}
+
+    has_secure_password
   validates :password, presence: true, length: {minimum: 7}
 
-  has_secure_password
+
 end
