346e869e8447d21e8b90e1e0039dd2c9433fcd28
shishkina <shishkina.kate@gmail.com>
2016-02-26 13:57:51 -0500

    updated ruby/rails versions

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,4 @@
 Rails.application.routes.draw do
-  get 'login' => "sessions#new"
-  post 'login' => "sessions#create"
-  delete 'logout' => "sessions#destroy"
-
-  root to: 'welcome#index'
-
-  resources :users do
-  resources :animals
-end
-resources :animals do
-  resources :comments
-
-end
-
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
