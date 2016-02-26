bd000506f88be41fc4ca27b73b84be662ecdb7f9
shishkina <shishkina.kate@gmail.com>
2016-02-26 15:34:58 -0500

    fixing routes after the update

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+
+ get 'login' => "sessions#new"
+ post 'login' => "sessions#create"
+ delete 'logout' => "sessions#destroy"
+ root to: 'welcome#index'
+ resources :users
+ resources :animals do
+ resources :comments
+ end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
